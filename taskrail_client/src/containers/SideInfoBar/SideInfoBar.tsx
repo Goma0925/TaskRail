a3745b57f63efae4533b9ed814701e5cdf09ad05
Goma0925 <>
2021-03-08 15:36:31 -0500

    Restored taskrail_client/src/containers/SideInfoBar/SideInfoBar.tsx

---
+++ taskrail_client/src/containers/SideInfoBar/SideInfoBar.tsx
@@ -0,0 +1,28 @@
+// Michael
+import React, { useState } from "react";
+import "./style.css";
+
+// Typescript uses interfaces (static, compile-time checking)
+// We also have PropTypes by React.js which does run-time type checking
+interface SideInfoBarProps {
+    // subtask: Subtask;
+}
+
+/** SideInfoBar's props must have the same
+shape as the Props interface object **/ 
+function SideInfoBar(props: SideInfoBarProps) {
+    // const [text, setText] = useState(props?.text); 
+    const [text, setText] = useState(""); 
+
+    function handleChange(event: any) { // update this type in future 
+        setText(event.target.value);
+    }
+
+    return (
+        <div className="sideinfo-bar">
+            <textarea value={text} onChange={handleChange} />
+        </div>
+    );
+}
+
+export default SideInfoBar; 
