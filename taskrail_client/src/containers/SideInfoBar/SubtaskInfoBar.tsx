b0d3aaf36f79b92eaadd440e549299dcbed85caf
JibraanKhan <jibraan.khan@centre.edu>
2021-03-08 18:17:51 -0500

    SubtaskInfoBar & UpdateSubtask (Redux)

--- taskrail_client/src/containers/SideInfoBar/SubtaskInfoBar.tsx
+++ taskrail_client/src/containers/SideInfoBar/SubtaskInfoBar.tsx
@@ -3,22 +3,30 @@ import React, { useState } from "react";
 import Subtask from "../../models/Subtask";
 import TaskParent from "../../models/TaskParent";
 import "./style.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { UpdateSubtask } from "../../redux/modules/TaskData/TaskDataActions";
 
 // Typescript uses interfaces (static, compile-time checking)
 // We also have PropTypes by React.js which does run-time type checking
 interface SideInfoBarProps {
-    taskParent?: TaskParent;
-    subtask?: Subtask;
+    taskParent: TaskParent;
+    subtask: Subtask;
 }
 
 /** SideInfoBar's props must have the same
 shape as the Props interface object **/ 
 function SideInfoBar(props: SideInfoBarProps) {
     // const [text, setText] = useState(props?.text); 
+    const subtask = props.subtask;
+
+    console.log(subtask);
     const [text, setText] = useState(""); 
 
+    const dispatch = useDispatch();
     function handleChange(event: any) { // update this type in future 
         setText(event.target.value);
+        dispatch(new UpdateSubtask(subtask));
     }
 
     return (
