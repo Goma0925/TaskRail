d2486c36f601439b9fcc6a9022fc8c0fa1aaf0dc
Noah Feld <noah.feld@centre.edu>
2021-03-17 17:34:02 -0400

    displayed and styled task parent node; still needs interatctivity support

---
+++ taskrail_client/src/components/TaskNode/Decorators/WithParentSkin/WithParentSkin.tsx
@@ -0,0 +1,30 @@
+import {TaskNodeProps} from "../../TaskNode"
+import {produce} from "immer";
+import TaskParent from "../../../../models/TaskParent";
+import "./WithParentSkin.css";
+
+interface WithParentSkinProps{
+    taskparent: TaskParent;
+}
+
+export default function WithSubtaskSkin(NodeToDecorate: React.ComponentType<TaskNodeProps&WithParentSkinProps>) 
+{
+    const wrapperComponent = (props: TaskNodeProps&WithParentSkinProps) => {
+        // Copy the props and append a new skin class.
+        var newProps = produce(props, draftProps=>{
+            draftProps.className = "parent-skin";
+            draftProps.className += props.className? props.className: "";
+        })
+        console.log("in WithParentSkin", newProps);
+        
+        return (
+            <NodeToDecorate {...newProps}>
+                {newProps.children}
+                <div className="taskparent-skin" style={{height:25}}>
+                    Name: {props.taskparent.getName()};
+                </div>
+            </NodeToDecorate>
+        )
+    }
+    return wrapperComponent;
+}
