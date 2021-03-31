90c93eb59f7d4d1ca982fc592b96c593ffb8b384
Goma0925 <>
2021-03-31 11:59:32 -0400

    Implemented a simple JSON return

--- taskrail_server/routes/workspace.js
+++ taskrail_server/routes/workspace.js
@@ -3,12 +3,26 @@ const mongoUtil = require("../MongoUtil");
 const db = mongoUtil.getDb();
 const workspaceRouter = express.Router();
 
-workspaceRouter.get('/workspace', (req, res) => {
+workspaceRouter.get('users/:user_id/workspace', (async (req, res) => {
     console.log("workspace root");
-    const collection = db.collection("TaskRail");
-    collection.forEach(element => {
-        console.log(element);
-    });
-});
+    const userId = req.params.id;
+    const collection = db.collection("Subtask");
+    const cursor = collection.find()
+    const arr = await cursor.toArray();
+    console.log(arr);
+    res.send(arr);
+}));
+
+workspaceRouter.post('users/:user_id/workspace', (async (req, res) => {
+    // Create a workspace under the user.
+    const userId = req.params.id;
+    const doc = { name: "New Workspace"};
+    const result = await collection.insertOne(doc);
+}));
+
+workspaceRouter.get("users/:user_id/workspace/:workspace_id", (async (req, res)=>{
+    // Return workspace with task parents nested inside of it.
+    const workspaceId = req.params.workspaceId;
+}));
 
 module.exports = workspaceRouter;
